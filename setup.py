d96a5cf3b682ffb0d1dc63a0d62946e7c0a2f82f
Adrielle Ayumi de Vasconcelos <adriayumi@gmail.com>
2019-03-04 20:28:48 -0300

    added pandas version, keywords and classifiers

--- setup.py
+++ setup.py
@@ -5,10 +5,20 @@ setup(
     version='0.1.0',
     packages=find_packages(include=['tspex']),
     license='GNU General Public License v3.0',
-    description='A Python package for calculating tissue-specificity metrics for gene expression',
+    description='A Python package for calculating tissue-specificity metrics for gene expression.',
     long_description=open('README.md').read(),
-    install_requires=['numpy', 'pandas'],
+    install_requires=['numpy', 'pandas >= 0.23'],
     url='https://github.com/apcamargo/tspex',
+    keywords=['gene expression', 'tissue-specificity', 'transcriptomics'],
     author='Antonio Pedro Camargo',
-    author_email='antoniop.camargo@gmail.com'
+    author_email='antoniop.camargo@gmail.com',
+    classifiers=[
+        'Development Status :: ',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3',
+    ]
 )
