f866fdd5dc7bbd9b23e192799dab5e5459eb7071
Adrielle Ayumi de Vasconcelos <adriayumi@gmail.com>
2019-03-10 14:45:32 -0300

    added header

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   This file is part of the tspex package, available at:
+#   https://github.com/apcamargo/tspex
+#
+#   Tspex is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#   Contact: antoniop.camargo@gmail.com
+
+"""The setup script."""
+
 from setuptools import setup, find_packages
 
 setup(
@@ -8,6 +31,7 @@ setup(
     description='A Python package for calculating tissue-specificity metrics for gene expression.',
     long_description=open('README.md').read(),
     install_requires=['numpy', 'pandas >= 0.23'],
+    python_requires= '>=3',
     url='https://github.com/apcamargo/tspex',
     keywords=['gene expression', 'tissue-specificity', 'transcriptomics'],
     author='Antonio Pedro Camargo',
@@ -20,5 +44,5 @@ setup(
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python :: 3',
-    ]
+    ],
 )
