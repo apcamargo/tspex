46eac9566b73b3c39024f1b86aea99affc85cd5f
Antonio Camargo <antoniop.camargo@gmail.com>
2019-04-17 08:55:23 -0300

    update CLI interface and move file

--- cli.py
+++ cli.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+#   This file is part of the tspex package, available at:
+#   https://github.com/apcamargo/tspex
+#
+#   Tspex is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+#   Contact: antoniop.camargo@gmail.com
+
+"""
+Command-line interface for tspex.
+"""
+
 import argparse
 import sys
 
@@ -5,34 +29,42 @@ import pandas as pd
 import tspex
 
 
-def main(input_file, output_file, method, log, transform, threshold):
+def main(input_file, output_file, method, log, disable_transformation, threshold):
     """Compute gene tissue-specificity from a expression matrix file."""
+    transform = not disable_transformation
     expression_matrix = pd.read_csv(input_file, index_col=0, header=0, sep=None, engine='python')
-    tissue_specificity = tspex.TissueSpecificity(expression_matrix, method, log, method, threshold)
+    tissue_specificity = tspex.TissueSpecificity(
+        expression_matrix, method, log, transform=transform, threshold=threshold)
     tissue_specificity.tissue_specificity.to_csv(output_file, sep='\t')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Classify sequences from a input FASTA file.',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    method_choices = ['counts', 'tsi', 'tau', 'gini', 'simpson', 'shannon_specificity',
+                      'roku_specificity', 'zscore', 'spm', 'spm_dpm', 'js_specificity',
+                      'js_specificity_dpm']
+    parser = argparse.ArgumentParser(
+        description='Compute gene tissue-specificity from a expression matrix file.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('input_file',
-                        help='expression matrix file in the TSV or CSV format.')
+                        help='Expression matrix file in the TSV or CSV format.')
     parser.add_argument('output_file',
-                        help='output TSV file containing the tissue-specificity values.')
-    parser.add_argument('method',
-                        choices=['counts','tsi', 'tau', 'gini', 'simpson', 'shannon_specificity',
-                                 'roku_specificity', 'zscore', 'spm', 'spm_dpm', 'js_specificity',
-                                 'js_specificity_dpm'],
-                        help='tissue-specificity metric.')
-    parser.add_argument('--log',
+                        help='Output TSV file containing tissue-specificity values.')
+    parser.add_argument(
+        'method', choices=method_choices, metavar='method',
+        help='Tissue-specificity metric. Allowed values are: "' + '", "'.join(method_choices) + '".')
+    parser.add_argument('-l', '--log',
                         action='store_true',
-                        help='foo.')
-    parser.add_argument('--disable_transformation',
-                        action='store_false', dest='transform',
-                        help='foo.')
-    parser.add_argument('--threshold',
-                        default=0, type=int,
-                        help='foo.')
+                        help='Log-transform expression values.')
+    parser.add_argument(
+        '-d', '--disable_transformation', action='store_true',
+        help=('By default, tissue-specificity values are transformed so that they range from 0 '
+              '(perfectly ubiquitous) to 1 (perfectly tissue-specific). If this parameter is used, '
+              'transformation will be disabled and each metric will have have a diferent range of '
+              'values.'))
+    parser.add_argument(
+        '-t', '--threshold', default=0, type=int,
+        help=('Threshold to use with the "counts" metric. If another method if chosen, this '
+              'parameter will be ignored.'))
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit(1)
